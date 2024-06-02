352732b70ac5371d68adecf7f5d9d148b5fd489c
b325890895 <batyablau@gmail.com>
2024-06-02 23:59:37 +0300

    sunday

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, NavLink, Switch } from 'react-router-dom';
 import './App.css';
 import Connect from './pages/Connect';
+import LandingPage from './pages/LandingPage';
 import Profile from './pages/Profile';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -8,11 +11,18 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
 function App() {
-
-
   return (
     <>
+    <BrowserRouter>
+    <Router>
+        <Route  path="/" component={LandingPage} />
+        
+        <Route  path="/profile" component={Profile} />
+
+      
+    </Router>
     
+    </BrowserRouter>
     {/* <Connect/> */}
     <Profile/>
     </>
