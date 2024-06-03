f33f3d5f7ae41c41a203a39cd3b04986e035a9db
yopsitsme <chpkats@gmail.com>
2024-06-04 00:55:23 +0300

    navBar with mui

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -16,23 +16,127 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import { Link, Outlet } from "react-router-dom";
 
-const drawerWidth = 240;
-const navItems = [
-  { name: "תשלום", link: "payment" },
-  { name: "התחייבות חברת החולים", link: "HMO" },
-  { name: "לוח שנה", link: "calendar" }
-];
+// const drawerWidth = 240;
+// // const navItems = [
+// //   { name: "תשלום", link: "payment" },
+// //   { name: "התחייבות חברת החולים", link: "HMO" },
+// //   { name: "לוח שנה", link: "calendar" }
+// // ];
+// const navItems = ['Home', 'About', 'Contact'];
 
-function DrawerAppBar() {
+
+// function DrawerAppBar() {
+
+//   const handleDrawerToggle = () => {
+//     setMobileOpen((prevState) => !prevState);
+//   };
+
+
+//   return (
+//     <>
+//     {/* <Box sx={{ display: "flex" }}>
+//       <CssBaseline />
+//       <AppBar component="nav">
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: "none" } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography
+//             variant="h6"
+//             component="div"
+//             sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+//           >
+//             לוגו
+//           </Typography>
+//           <Box sx={{ display: { xs: "none", sm: "block" } }}>
+//             {navItems.map((item) => (
+//               <Button key={item.link} sx={{ color: "#fff" }}>
+//                 <Link to={item.link.toLowerCase()}>{item.name}:{item.link}</Link>
+//               </Button>
+//             ))}
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//     </Box>
+//      */}
+
+
+//     <Box sx={{ display: 'flex' }}>
+//       <CssBaseline />
+//       <AppBar component="nav">
+//         <Toolbar>
+//           <IconButton
+//             color="inherit"
+//             aria-label="open drawer"
+//             edge="start"
+//             onClick={handleDrawerToggle}
+//             sx={{ mr: 2, display: { sm: 'none' } }}
+//           >
+//             <MenuIcon />
+//           </IconButton>
+//           <Typography
+//             variant="h6"
+//             component="div"
+//             sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+//           >
+//             MUI
+//           </Typography>
+//           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+//             {navItems.map((item) => (
+//               <Button key={item} sx={{ color: '#fff' }}>
+//                 {item}
+//               </Button>
+//             ))}
+//           </Box>
+//         </Toolbar>
+//       </AppBar>
+//       </Box>
+
+//       <Outlet/>
+//     </>
+//   );
+// }
+
+
+const drawerWidth = 240;
+const navItems = ['בית', 'About', 'Contact'];
+
+function DrawerAppBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
   const handleDrawerToggle = () => {
     setMobileOpen((prevState) => !prevState);
   };
 
+  // const drawer = (
+  //   <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+  //     <Typography variant="h6" sx={{ my: 2 }}>
+  //       MUI
+  //     </Typography>
+  //     <Divider />
+  //     <List>
+  //       {navItems.map((item) => (
+  //         <ListItem key={item} disablePadding>
+  //           <ListItemButton sx={{ textAlign: 'center' }}>
+  //             <ListItemText primary={item} />
+  //           </ListItemButton>
+  //         </ListItem>
+  //       ))}
+  //     </List>
+  //   </Box>
+  // );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
 
   return (
-    <>
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar component="nav">
         <Toolbar>
@@ -41,30 +145,49 @@ function DrawerAppBar() {
             aria-label="open drawer"
             edge="start"
             onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { sm: "none" } }}
+            sx={{ mr: 2, display: { sm: 'none' } }}
           >
             <MenuIcon />
           </IconButton>
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
           >
-            לוגו
+            MUI
           </Typography>
-          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item) => (
-              <Button key={item.link} sx={{ color: "#fff" }}>
-                <Link to={item.link.toLowerCase()}>{item.name}:{item.link}</Link>
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
               </Button>
             ))}
           </Box>
         </Toolbar>
       </AppBar>
+      {/* <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav> */}
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+      </Box>
     </Box>
-      <Outlet/>
-    </>
   );
 }
+
 
 export default DrawerAppBar;
