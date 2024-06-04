c49debe90f4cbb32132036d454b2da1f92567679
b325890895 <batyablau@gmail.com>
2024-06-04 03:23:26 +0300

    celender

--- client/src/pages/Calendar.jsx
+++ client/src/pages/Calendar.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import dayjs from 'dayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
 
+  // const [value, setValue] = React.useState(dayjs('2022-04-17'));
 function Calendar() {
-  return (<div>לוח שנה</div>);
+  return (<>
+   {/* <DateCalendar defaultValue={dayjs('2022-04-17')} /> */}
+   {/* <DateCalendar value={value} onChange={(newValue) => setValue(newValue)} /> */}
+   <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateCalendar', 'DateCalendar']}>
+        <DemoItem label="Uncontrolled calendar">
+          <DateCalendar defaultValue={dayjs('2022-04-17')}views={['day']} onClick={(newValue)=>{alert({newValue})}} />
+        </DemoItem>
+      </DemoContainer>
+    </LocalizationProvider>
+  </>
+  );
 }
 
 export default Calendar;
