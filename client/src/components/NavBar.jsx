a4338cd5af938e60d94a7ed66f51362b7e96d198
yopsitsme <chpkats@gmail.com>
2024-07-03 07:11:21 +0300

    client calender + css updates

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,153 @@
-import React,{useEffect} from "react";
+// import React, { useState } from "react";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import Toolbar from "@mui/material/Toolbar";
+// import Typography from "@mui/material/Typography";
+// import Drawer from '@mui/material/Drawer';
+// import Button from "@mui/material/Button";
+// import { Link, Outlet } from "react-router-dom";
+// import "./components_css/NavBar.css";
+// import { useNavigate } from "react-router-dom";
+
+// function NavBar({ userId, userState }) {
+//   const navigate = useNavigate();
+//   const [mobileOpen, setMobileOpen] = useState(false);
+//   const navItemsClient = [
+//     { name: "בית", link: "home" },
+//     // { name: "תשלום", link: "payment" },
+//     // { name: "התחייבות חברת החולים", link: "hmo/" },
+//     { name: "לוח שנה", link: `calendar/${userId}` },
+//     { name: "אזור אישי", link: `profile/${userId}` },
+//   ];
+//   const navItemsManager = [
+//     { name: "בית", link: "home" },
+//     // { name: "צפיה בקבלות", link: "payment" },
+//     { name: "צפיה בלקוחות", link: "clients" },
+//     { name: "לוח שנה", link: `calendar/${userId}` },
+//     // { name: "אזור אישי", link: "profile" },
+//   ];
+//   let navItems = null;
+
+//   switch (userState) {
+//     case "manager":
+//       navItems = navItemsManager;
+//       break;
+//     case "client":
+//       navItems = navItemsClient;
+//       break;
+//     default:
+//       navItems = null;
+//       navigate("/*");
+//       break;
+//   }
+
+//   const handleDrawerToggle = () => {
+//     setMobileOpen(!mobileOpen);
+//   };
+
+//   const drawer = (
+//     <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+//       <Typography variant="h6" sx={{ my: 2 }}>
+//         <img
+//           id="logoWithText"
+//           src="../src/assets/logoWithText.jpg"
+//           alt="logo with text"
+//           style={{ width: '100px' }}
+//         />
+//       </Typography>
+//       <Box>
+//         {navItems &&
+//           navItems.map((item) => (
+//             <Button key={item.link} sx={{ color: "black" }}>
+//               <Link
+//                 to={item.link.toLowerCase()}
+//                 style={{
+//                   color: "black",
+//                   textDecoration: "none",
+//                 }}
+//               >
+//                 {item.name}
+//               </Link>
+//             </Button>
+//           ))}
+//       </Box>
+//     </Box>
+//   );
+
+//   return (
+//     <>
+//       <Box sx={{ display: "flex" }} dir="rtl">
+//         <CssBaseline />
+//         <AppBar
+//           component="nav"
+//           sx={{ backgroundColor: "#aee62d", color: "black" }}
+//         >
+//           <Toolbar>
+//             <IconButton
+//               color="inherit"
+//               aria-label="open drawer"
+//               edge="start"
+//               onClick={handleDrawerToggle}
+//               sx={{ mr: 2, display: { sm: "none" } }}
+//             >
+//               <MenuIcon />
+//             </IconButton>
+//             <Typography
+//               variant="h6"
+//               component="div"
+//               sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+//             >
+//               <img
+//                 id="logoWithText"
+//                 src="../src/assets/logoWithText.jpg"
+//                 alt="logo with text"
+//               />
+//             </Typography>
+//             <Box sx={{ display: { xs: "none", sm: "block" } }}>
+//               {navItems &&
+//                 navItems.map((item) => (
+//                   <Button key={item.link} sx={{ color: "black" }}>
+//                     <Link
+//                       to={item.link.toLowerCase()}
+//                       style={{
+//                         color: "black",
+//                         textDecoration: "none",
+//                       }}
+//                     >
+//                       {item.name}
+//                     </Link>
+//                   </Button>
+//                 ))}
+//             </Box>
+//           </Toolbar>
+//         </AppBar>
+//         <Box component="nav">
+//           <Drawer
+//             variant="temporary"
+//             open={mobileOpen}
+//             onClose={handleDrawerToggle}
+//             ModalProps={{
+//               keepMounted: true, // Better open performance on mobile.
+//             }}
+//             sx={{
+//               display: { xs: 'block', sm: 'none' },
+//               '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+//             }}
+//           >
+//             {drawer}
+//           </Drawer>
+//         </Box>
+//         <Outlet />
+//       </Box>
+//     </>
+//   );
+// }
+
+// export default NavBar;
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -6,15 +155,15 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Drawer from '@mui/material/Drawer';
 import Button from "@mui/material/Button";
 import { Link, Outlet } from "react-router-dom";
 import "./components_css/NavBar.css";
 import { useNavigate } from "react-router-dom";
 
-
-function NavBar({ userId,userState }) {
+function NavBar({ userId, userState }) {
   const navigate = useNavigate();
-
+  const [mobileOpen, setMobileOpen] = useState(false);
   const navItemsClient = [
     { name: "בית", link: "home" },
     // { name: "תשלום", link: "payment" },
@@ -26,30 +175,56 @@ function NavBar({ userId,userState }) {
     { name: "בית", link: "home" },
     // { name: "צפיה בקבלות", link: "payment" },
     { name: "צפיה בלקוחות", link: "clients" },
-    { name: "לוח שנה", link: "calendar" },
+    { name: "לוח שנה", link: `calendar/${userId}` },
     // { name: "אזור אישי", link: "profile" },
   ];
   let navItems = null;
-  
 
-    switch (userState) {
-      case "manager":
-        navItems = navItemsManager;
-        break;
-      case "client":
-        navItems = navItemsClient;
-        break;
-      default:
-        navItems = null;
-        navigate("/*");
-        break;
-    }
-  
-
+  switch (userState) {
+    case "manager":
+      navItems = navItemsManager;
+      break;
+    case "client":
+      navItems = navItemsClient;
+      break;
+    default:
+      navItems = null;
+      navigate("/*");
+      break;
+  }
 
   const handleDrawerToggle = () => {
-    setMobileOpen((prevState) => !prevState);
+    setMobileOpen(!mobileOpen);
   };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        <img
+          id="logoWithText"
+          src="../src/assets/logoWithText.jpg"
+          alt="logo with text"
+          style={{ width: '100px' }}
+        />
+      </Typography>
+      <Box>
+        {navItems &&
+          navItems.map((item) => (
+            <Button key={item.link} sx={{ color: "black" }}>
+              <Link
+                to={item.link.toLowerCase()}
+                style={{
+                  color: "black",
+                  textDecoration: "none",
+                }}
+              >
+                {item.name}
+              </Link>
+            </Button>
+          ))}
+      </Box>
+    </Box>
+  );
 
   return (
     <>
@@ -98,10 +273,30 @@ function NavBar({ userId,userState }) {
             </Box>
           </Toolbar>
         </AppBar>
-        <Outlet />
+        <Box component="main" sx={{ flexGrow: 1, paddingTop: '60px' }}>
+          {/* כאן נשים את כל התוכן הראשי של הדף */}
+          <Outlet />
+        </Box>
+        <Box component="nav">
+          <Drawer
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Box>
       </Box>
     </>
   );
 }
 
 export default NavBar;
+
--- client/src/pages/ViewClient.jsx
+++ client/src/pages/ViewClient.jsx
@@ -1,21 +1,125 @@
+// import React, { useState, useEffect } from "react";
+// import { useParams, useNavigate } from "react-router-dom";
+// import HourAndDate from "../components/HourAndDate";
+// import ClientDetails from "../components/ClientDetails";
+// // import Loading from "./Loading"
+// // import Error  from "./Error"
+// import Alert from "@mui/material/Alert";
+// import Stack from "@mui/material/Stack";
+// import Button from "@mui/material/Button";
+// import EditIcon from "@mui/icons-material/Edit";
+// import Typography from "@mui/material/Typography";
+
+
+// function ViewClient({ userState }) {
+//   const clientId = useParams().id;
+//   const [isLoading, setIsLoading] = useState(true);
+//   const [fetchError, setFetchError] = useState(null);
+//   const [hourDateAlert, setHourDateAlert] = useState(false);
+//   const [hourDateUpdate, setHourDateUpdate] = useState(false);
+//   const [client, setClient] = useState();
+//   const navigate = useNavigate();
+//   const URL_API = "http://localhost:3000";
+//   switch (userState) {
+//     case "manager":
+//       break;
+//     case "client":
+//       navigate("/*");
+//       break;
+//     default:
+//       navigate("/*");
+//       break;
+//   }
+
+//   useEffect(() => {
+//     fetchClient();
+//   }, []);
+
+//   const fetchClient = async () => {
+//     try {
+//       const response = await fetch(`${URL_API}/user/${clientId}`);
+//       if (!response.ok) {
+//         throw Error("Did not received clients data from server");
+//       }
+//       const result = await response.json();
+//       setClient(result);
+//       if (!result.day) {
+//         setHourDateAlert(true);
+//       }
+//     } catch (err) {
+//       setFetchError(err.message);
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+//   function updateButtonHandler() {
+//     setHourDateAlert(false);
+//     setHourDateUpdate(true);
+//   }
+//   return (
+//     <>
+//       <Stack sx={{ width: "100%" }} spacing={2}>
+//         {/* {isLoading && <Loading />} */}
+//         {/* {fetchError && <Error message={fetchError} />} */}
+//         {!isLoading && !fetchError && <ClientDetails client={client} />}
+//         {!isLoading && !fetchError && hourDateAlert && (
+//           <Alert
+//             dir="rtl"
+//             severity="warning"
+//             action={
+//               <Button
+//                 color="inherit"
+//                 size="small"
+//                 onClick={updateButtonHandler}
+//               >
+//                 <EditIcon />
+//               </Button>
+//             }
+//           >
+//             ללקוח לא מוגדר יום ושעה לטיפול.
+//           </Alert>
+//         )}
+//         {!isLoading && !fetchError && !hourDateAlert && (
+//           <div dir="rtl">
+//             <Typography component="h1" variant="h4">
+//               טיפול מתקיים ב:{" "}
+//             </Typography>
+//             <Typography component="h1" variant="h6">
+//               יום {client.day}{" "}
+//             </Typography>
+//             <Typography component="h1" variant="h6">
+//               בשעה {client.time}
+//             </Typography>
+//           </div>
+//         )}
+//         {!isLoading && !fetchError && hourDateUpdate && (
+//           <HourAndDate
+//             clientId={client.user_id}
+//             setHourDateUpdate={setHourDateUpdate}
+//           />
+//         )}
+//       </Stack>
+//     </>
+//   );
+// }
+
+// export default ViewClient;
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import HourAndDate from "../components/HourAndDate"
+import HourAndDate from "../components/HourAndDate";
 import ClientDetails from "../components/ClientDetails";
-// import Loading from "./Loading"
-// import Error  from "./Error"
-import Alert from '@mui/material/Alert';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import EditIcon from '@mui/icons-material/Edit';
-
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import EditIcon from "@mui/icons-material/Edit";
+import Typography from "@mui/material/Typography";
 
 function ViewClient({ userState }) {
   const clientId = useParams().id;
   const [isLoading, setIsLoading] = useState(true);
   const [fetchError, setFetchError] = useState(null);
-  const [hourDateAlert,setHourDateAlert] = useState(false)
-  const [hourDateUpdate,setHourDateUpdate] = useState(false)
+  const [hourDateAlert, setHourDateAlert] = useState(false);
+  const [hourDateUpdate, setHourDateUpdate] = useState(false);
   const [client, setClient] = useState();
   const navigate = useNavigate();
   const URL_API = "http://localhost:3000";
@@ -38,12 +142,12 @@ function ViewClient({ userState }) {
     try {
       const response = await fetch(`${URL_API}/user/${clientId}`);
       if (!response.ok) {
-        throw Error("Did not received clients data from server");
+        throw Error("Did not receive client data from server");
       }
       const result = await response.json();
       setClient(result);
-      if(!result.day){
-        setHourDateAlert(true)
+      if (!result.day) {
+        setHourDateAlert(true);
       }
     } catch (err) {
       setFetchError(err.message);
@@ -51,35 +155,54 @@ function ViewClient({ userState }) {
       setIsLoading(false);
     }
   };
- function updateButtonHandler(){
-  setHourDateAlert(false);
-  setHourDateUpdate(true);
 
- }
-   return (
+  function updateButtonHandler() {
+    setHourDateAlert(false);
+    setHourDateUpdate(true);
+  }
+
+  return (
     <>
-        <Stack sx={{ width: '100%' }} 
-        spacing={1}
-        >
-
-      {/* {isLoading && <Loading />} */}
-      {/* {fetchError && <Error message={fetchError} />} */}
-      {!isLoading && !fetchError && (
-        <ClientDetails client={client}/>
-      )}
-      {
-        !isLoading && !fetchError && hourDateAlert &&
-        <Alert dir="rtl" severity="warning"      action={
-          <Button color="inherit" size="small" onClick={updateButtonHandler}>
-            <EditIcon/>
-          </Button>
-        }>
-     ללקוח לא מוגדר יום ושעה לטיפול.
-      </Alert>  
-      }{
-        !isLoading && !fetchError && hourDateUpdate &&
-        <HourAndDate clientId={client.user_id} setHourDateUpdate={setHourDateUpdate}/>
-      }
+      <Stack sx={{ width: "100%" }} spacing={2} className="custom-container">
+        {/* {isLoading && <Loading />} */}
+        {/* {fetchError && <Error message={fetchError} />} */}
+        {!isLoading && !fetchError && <ClientDetails client={client} />}
+        {!isLoading && !fetchError && hourDateAlert && (
+          <Alert
+            dir="rtl"
+            severity="warning"
+            action={
+              <Button
+                color="inherit"
+                size="small"
+                onClick={updateButtonHandler}
+              >
+                <EditIcon />
+              </Button>
+            }
+          >
+            ללקוח לא מוגדר יום ושעה לטיפול.
+          </Alert>
+        )}
+        {!isLoading && !fetchError && !hourDateAlert && (
+          <div dir="rtl">
+            <Typography component="h1" variant="h4">
+              טיפול מתקיים ב:{" "}
+            </Typography>
+            <Typography component="h1" variant="h6">
+              יום {client.day}{" "}
+            </Typography>
+            <Typography component="h1" variant="h6">
+              בשעה {client.time}
+            </Typography>
+          </div>
+        )}
+        {!isLoading && !fetchError && hourDateUpdate && (
+          <HourAndDate
+            clientId={client.user_id}
+            setHourDateUpdate={setHourDateUpdate}
+          />
+        )}
       </Stack>
     </>
   );
